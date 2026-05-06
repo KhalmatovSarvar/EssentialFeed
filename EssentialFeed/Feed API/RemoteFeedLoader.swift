8985ae0b89247c1a89c91f55dccf19ca3a7ce00b
Sarvarbek <khalmatovsarvar5779@gmail.com>
2026-05-06 14:31:07 +0500

    Initial project setup with clean structure

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,38 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Sarvar on 14/01/26.
+//
+
+import Foundation
+
+public final class RemoteFeedLoader: FeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = LoadFeedResult
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard let self = self else { return }
+
+            switch result {
+            case let .success(data, response):
+                completion(FeedItemsMapper.map(data, from: response))
+            case .failure:
+                completion(.failure(RemoteFeedLoader.Error.connectivity))
+            }
+        }
+    }
+}
